739ba39b14fe68aecc73381fb0a19a6d9caa04b5
BlastPy <blast.py@gmail.com>
2015-05-21 19:19:46 +0300

    Динамічні закладки в горіє

--- wiki/views.py
+++ wiki/views.py
@@ -9,26 +9,32 @@ from wiki.models import Stats
 
 
 def index(request):
-    namep = NamePart.objects.all()
-    rozdil = Rozdil.objects.all()
-    context = {'rozdil': rozdil , 'namepart':namep }
+    foo = []
+    for name_part in NamePart.objects.all():
+        foo.append({'name_part': name_part,'rozdils': Rozdil.objects.filter(part_of=name_part.pk)})
+
+    context = {'foo': foo }
 
     return render(request,'index.html',context)
 
 
 def all_rozdil(request,is_main):
-    namep = NamePart.objects.all()
+    foo = []
+    for name_part in NamePart.objects.all():
+        foo.append({'name_part': name_part,'rozdils': Rozdil.objects.filter(part_of=name_part.pk)})
     stats = Stats.objects.all().filter(main_is_id=is_main)
     rozdil = Rozdil.objects.all()
     title_rozdil = Rozdil.objects.get(pk=is_main)
-    context = {'stats':stats,'rozdil': rozdil,'title_rozdil':title_rozdil , 'namepart':namep }
+    context = {'stats':stats,'rozdil': rozdil,'title_rozdil':title_rozdil , 'foo': foo }
 
     return render(request,'stats.html',context)
 
 def detali(request,id_ekz):
-    namep = NamePart.objects.all()
+    foo = []
+    for name_part in NamePart.objects.all():
+        foo.append({'name_part': name_part,'rozdils': Rozdil.objects.filter(part_of=name_part.pk)})
     details = Stats.objects.get(pk=id_ekz)
     rozdil = Rozdil.objects.all()
-    context = {'details':details,'rozdil': rozdil,'namepart':namep}
+    context = {'details':details,'rozdil': rozdil,'foo': foo}
 
     return render(request,'details.html',context)
